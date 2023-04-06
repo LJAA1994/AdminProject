73bd000d6469a671160f6d2e4634821ed2ec3e55
LJAA1994 <lucas.teleco.info@gmail.com>
2023-04-06 16:26:28 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'dashboard',
+  component:DashboardComponent
+},
+{
+  path:'login',
+  component:LoginComponent
+},
+{
+  path:'register',
+  component:RegisterComponent
+},
+{
+  path:'progress',
+  component:ProgressComponent
+},
+{
+  path:'',
+  component:DashboardComponent
+},
+{
+  path:'dashboard',
+  component:DashboardComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
